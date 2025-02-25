98f5aa0733bb58d627994eba514a5eb3015f093d
GHAZI ALANZI <105205339+GHAZI-ALANZI@users.noreply.github.com>
2025-02-25 13:03:21 +0100

    init security and crud operations

---
+++ crud.py
@@ -0,0 +1,54 @@
+from sqlalchemy.orm import Session
+from models import User
+from schemas import UserCreate
+from security import get_password_hash, verify_password
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(User).filter(User.email == email).first()
+
+def get_user_by_username(db: Session, username: str):
+    return db.query(User).filter(User.username == username).first()
+
+def get_all_users(db: Session):
+    return db.query(User).all()
+
+def create_user(db: Session, user: UserCreate):
+    hashed_password = get_password_hash(user.password)
+    db_user = User(
+        username=user.username,
+        email=user.email,
+        hashed_password=hashed_password,
+        role=user.role
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def authenticate_user(db: Session, username: str, password: str):
+    user = get_user_by_username(db, username)
+    if not user or not verify_password(password, user.hashed_password):
+        return None
+    return user
+
+def update_user(db: Session, user_id: int, updated_user: UserCreate):
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        return None
+
+    user.username = updated_user.username
+    user.email = updated_user.email
+    user.role = updated_user.role
+    user.hashed_password = get_password_hash(updated_user.password)
+    
+    db.commit()
+    db.refresh(user)
+    return user
+
+def delete_user(db: Session, user_id: int):
+    user = db.query(User).filter(User.id == user_id).first()
+    if user:
+        db.delete(user)
+        db.commit()
+        return True
+    return False
